f6510e5e10c7e8968bd5a39144928deff01ea672
TheUnknown550 <mattcosh06@gmail.com>
2025-10-31 15:14:34 +0700

    Calculate rich percentage for minimum work hours in calculate_demographic_data function

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -36,7 +36,9 @@ def calculate_demographic_data(print_data=True):
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
     num_min_workers = df[df['hours-per-week'] == min_work_hours]
 
-    rich_percentage = None
+    rich_percentage = round(
+        (num_min_workers[num_min_workers['salary'] == '>50K'].shape[0] /
+         num_min_workers.shape[0]) * 100, 1)
 
     # What country has the highest percentage of people that earn >50K?
     highest_earning_country = None
